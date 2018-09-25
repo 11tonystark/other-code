c88cfaa0ab98563099a549e4664712ea57f3a307
11tonystark <abhishukla7732@gmail.com>
2018-09-25 22:15:07 +0530

    bms arduino

---
+++ adc_bms_0-24v/adc_bms_0-24v.ino
@@ -0,0 +1,40 @@
+
+int sensorPin = A0;    
+int ledPin = 13;      
+double sensorValue = 0;  
+double val=0;
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  pinMode(ledPin, OUTPUT);  
+}
+
+//Map function
+ double map( double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+void loop() {
+ 
+  sensorValue = analogRead(sensorPin);  
+  val = map(sensorValue,0,1023,0,24);
+  
+  Serial.println(sensorValue); 
+  
+ //Serial.println();
+  Serial.println(val,8);  
+  Serial.println();
+  delay(1000); 
+  
+  digitalWrite(ledPin, HIGH);  
+ 
+  delay(sensorValue);          
+        
+  digitalWrite(ledPin, LOW);   
+  
+  //delay(sensorValue);                  
+}
